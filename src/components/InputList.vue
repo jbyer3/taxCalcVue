f0edd0aabc02b889522263f636bc434ddb9a271d
Jason Byer <jbyer3@gmail.com>
2018-08-04 14:51:15 -0400

    add function to add up all the costs of items in the list and display it on the page

--- src/components/InputList.vue
+++ src/components/InputList.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="inputer">
-    <input v-model="message" v-on:keyup.enter="addItem()" placeholder="edit me">  
+    <input v-model="message" v-on:keyup.enter="addItem(), taxCalc()" placeholder="name of food">  
+    <input v-model="foods.cost" v-on:keyup.enter="addItem(), taxCalc()" placeholder="cost of food">  
     <button v-on:click="addItem()" type="submit">Add to list</button>
+
+    <h2>{{ totalCost }}</h2>
+    
+
     <ul>
       <li v-for="food in foods" :key="food.id">
-        {{ food.name }}
+        {{ food.name }} ${{ food.cost }}
       </li>
     </ul>
   </div>
@@ -16,19 +21,35 @@ export default {
   data() {
     return {
       foods: [
-        { name: "apples" }, 
-        { name: "carrots" }, 
-        { name: "onions" }
-        ],
+        { name: "apples", cost: 3.99 },
+        { name: "carrots", cost: 2.79 },
+        { name: "onions", cost: 8.99 }
+      ],
       totalCost: 0,
       message: ""
     };
   },
   methods: {
+    taxCalc: function() {
+      this.totalCost = 0;
+      let costArray = [];
+      for (let i = 0; i < this.foods.length; i++) {
+        costArray.push(this.foods[i].cost);
+      }
+      console.log(costArray);
+      this.totalCost = costArray.reduce((total, amount) => {
+        total += amount;
+        return total;
+      });
+    },
     addItem: function() {
-      let newFood = { name: this.message };
+      let newFood = {
+        name: this.message,
+        cost: Number(this.foods.cost)
+      };
       this.foods.push(newFood);
       this.message = "";
+      this.foods.cost = "";
     }
   }
 };
